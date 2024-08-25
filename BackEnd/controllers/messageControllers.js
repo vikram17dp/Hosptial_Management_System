591504526cda25b60509916f45f2a5ed8f2b59ad
vikram17dp <vikramdp505@gmail.com>
2024-08-25 23:42:43 +0530

    message controller and router

--- BackEnd/controllers/messageControllers.js
+++ BackEnd/controllers/messageControllers.js
@@ -13,3 +13,10 @@ export const sendMessage = AysncError(async (req,res,next)=>{
         message:"message sent succesfully"
     })
 })
+export const getAllMessages = AysncError(async(req,res,next)=>{
+    const messages = await Message.find();
+    res.status(200).json({
+        success:true,
+        messages
+    })
+})
--- BackEnd/router/messagerouter.js
+++ BackEnd/router/messagerouter.js
@@ -1,6 +1,8 @@
 import express from 'express'
-import {sendMessage} from '../controllers/messageControllers.js'
+import {getAllMessages, sendMessage} from '../controllers/messageControllers.js'
+import { isAdminAuthenicated } from '../middlewares/auth.js';
 const router = express.Router();
 
 router.post('/send',sendMessage);
+router.get('/getall',getAllMessages,isAdminAuthenicated)
 export default router;
