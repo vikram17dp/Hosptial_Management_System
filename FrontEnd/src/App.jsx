8b94462fdedd9b25ad687976eb7d391888ee359d
vikram17dp <vikramdp505@gmail.com>
2024-08-27 17:37:05 +0530

    Routes setups

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
 import './App.css'
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
+import Home from './pages/Home'
+import Appointment from './pages/Appointment'
+import AboutUs from './pages/AboutUs'
+import Register from './pages/Register'
+import Login from './pages/Login'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <Router>
+          <Routes>
+            <Route path='/' element={<Home/>}/>
+            <Route path='/appointment' element={<Appointment/>}/>
+            <Route path='/about' element={<AboutUs/>}/>
+            <Route path='/register' element={<Register/>}/>
+            <Route path='/login' element={<Login/>}/>
+          </Routes>
+          <ToastContainer position='top-center'/>
+
+        </Router> 
+    </>
   )
 }
 
--- FrontEnd/src/main.jsx
+++ FrontEnd/src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
+import { createContext, StrictMode, useState } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
 
+export const Context = createContext({isAuthenticated:false});
+
+const Appwrapeer = ()=>{
+    const [isAuthenticated,setIsAutenticated] = useState(false);
+    const [user,setUser] = useState();
+    <Context.Provider value={{isAuthenticated,setIsAutenticated,user,setUser}}>
+      <App/>
+    </Context.Provider>
+    
+
+}
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Appwrapeer />
   </StrictMode>,
 )
